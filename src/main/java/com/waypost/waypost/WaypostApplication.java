a281ef4e253c8b2578b8489c47268b3b817da781
dongyoon7212 <dongyoon7212@naver.com>
2025-03-25 16:56:33 +0900

    Init: backend init

---
+++ src/main/java/com/waypost/waypost/WaypostApplication.java
@@ -0,0 +1,13 @@
+package com.waypost.waypost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaypostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaypostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waypost/waypost/WaypostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waypost.waypost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaypostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
