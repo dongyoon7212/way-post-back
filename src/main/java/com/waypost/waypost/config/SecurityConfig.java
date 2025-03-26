ddb0ef10b6d7696e5a6338c1af260f896c27f3ba
dongyoon7212 <dongyoon7212@naver.com>
2025-03-26 17:08:00 +0900

    Feat: security

--- src/main/java/com/waypost/waypost/config/SecurityConfig.java
+++ src/main/java/com/waypost/waypost/config/SecurityConfig.java
@@ -1,5 +1,6 @@
-package com.waypost.waypost.security;
+package com.waypost.waypost.config;
 
+import com.waypost.waypost.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
