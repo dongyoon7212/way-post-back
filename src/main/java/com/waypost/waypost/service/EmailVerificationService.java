9c7e02fcd5ba3572b7857396350f0a1068ca51e4
dongyoon7212 <dongyoon7212@naver.com>
2025-05-28 15:51:56 +0900

    Feat: 템플릿 변경 및 인증 유효시간 변경

--- src/main/java/com/waypost/waypost/service/EmailVerificationService.java
+++ src/main/java/com/waypost/waypost/service/EmailVerificationService.java
@@ -26,7 +26,7 @@ public class EmailVerificationService {
 
     public void sendVerificationCode(int userId, String email) {
         String code = String.format("%06d", new Random().nextInt(999999));
-        LocalDateTime expiredDt = LocalDateTime.now().plusMinutes(5);
+        LocalDateTime expiredDt = LocalDateTime.now().plusMinutes(2);
 
         repository.invalidatePreviousCodes(userId);
 
@@ -41,8 +41,22 @@ public class EmailVerificationService {
             MimeMessageHelper helper = new MimeMessageHelper(message, false, "utf-8");
             helper.setTo(email);
             helper.setSubject("WayPost 이메일 인증 코드");
-            helper.setText("<div><h3>아래 인증 코드를 입력해주세요</h3><h2>" + code + "</h2><p>유효시간: 5분</p></div>", true);
-            mailSender.send(message);
+            helper.setText(
+                    "<div style=\"font-family: 'Arial', sans-serif; background-color: #f9f9f9; padding: 30px; border-radius: 10px; border: 1px solid #ddd; max-width: 500px; margin: auto;\">" +
+                            "<div style=\"text-align: center; margin-bottom: 20px;\">" +
+                            "<img src='https://firebasestorage.googleapis.com/v0/b/way-post-9f21b.firebasestorage.app/o/logo-assets%2Flogo1.png?alt=media&token=c1da33dc-5e94-4e5f-bb9b-112c749ae0c6' alt='WayPost 로고' style='height: 150px;'/>" +
+                            "</div>" +
+                            "<h2 style=\"color: #1E90FF; text-align: center;\">WayPost 이메일 인증</h2>" +
+                            "<p style=\"font-size: 16px; color: #333; text-align: center;\">아래 인증 코드를 입력하여 이메일 인증을 완료해주세요.</p>" +
+                            "<div style=\"margin: 30px auto; text-align: center; padding: 20px; border-radius: 8px; background-color: #f1f8ff; width: fit-content; border: 1px dashed #1E90FF;\">" +
+                            "<span style=\"font-size: 28px; font-weight: bold; letter-spacing: 4px; color: #1E90FF;\">" + code + "</span>" +
+                            "</div>" +
+                            "<p style=\"font-size: 14px; color: #666; text-align: center;\">인증 코드는 <strong>2분간 유효</strong>합니다.</p>" +
+                            "<hr style=\"margin: 30px 0;\">" +
+                            "<p style=\"font-size: 12px; color: #999; text-align: center;\">본 메일은 WayPost 인증용으로 발송되었으며, 회신하지 마세요.</p>" +
+                            "</div>",
+                    true
+            );            mailSender.send(message);
         } catch (Exception e) {
             e.printStackTrace();
         }
