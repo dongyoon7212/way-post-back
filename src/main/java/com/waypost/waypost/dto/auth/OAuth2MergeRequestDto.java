a8a7fec66df1029d83a5c1ec293ff940e0a54ded
dongyoon7212 <dongyoon7212@naver.com>
2025-06-03 14:45:06 +0900

    Feat: oauth2 연동 및 로그인 구현

--- src/main/java/com/waypost/waypost/dto/auth/OAuth2MergeRequestDto.java
+++ src/main/java/com/waypost/waypost/dto/auth/OAuth2MergeRequestDto.java
@@ -1,10 +1,20 @@
 package com.waypost.waypost.dto.auth;
 
+import com.waypost.waypost.entity.OAuth2User;
 import lombok.Data;
 
 @Data
 public class OAuth2MergeRequestDto {
     private String email;
+    private String password;
     private String provider;
     private String providerUserId;
+
+    public OAuth2User toOAuth2User(int userId) {
+        return OAuth2User.builder()
+                .userId(userId)
+                .provider(provider)
+                .providerUserId(providerUserId)
+                .build();
+    }
 }
--- src/main/java/com/waypost/waypost/service/OAuth2AuthService.java
+++ src/main/java/com/waypost/waypost/service/OAuth2AuthService.java
@@ -10,6 +10,8 @@ import com.waypost.waypost.repository.UserRepository;
 import com.waypost.waypost.repository.UserRoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -32,16 +34,15 @@ public class OAuth2AuthService {
     private BCryptPasswordEncoder passwordEncoder;
 
     public Map<String, Object> mergeAccount(OAuth2MergeRequestDto oAuth2MergeRequestDto) {
-        Optional<User> user = userRepository.findByEmail(oAuth2MergeRequestDto.getEmail());
-        if (user == null) {
-            return Map.of(
-                    "status", HttpStatus.BAD_REQUEST.value(),
-                    "code", 4001,
-                    "message", "존재하지 않는 이메일입니다."
-            );
+        User foundUser = userRepository.findByEmail(oAuth2MergeRequestDto.getEmail())
+                .orElseThrow(() -> new UsernameNotFoundException("사용자 정보를 확인하세요."));
+        if (!passwordEncoder.matches(oAuth2MergeRequestDto.getPassword(), foundUser.getPassword())) {
+            throw new BadCredentialsException("사용자 정보를 확인하세요.");
         }
 
-        userRepository.updateOAuth2Info(user.get().getUserId(), oAuth2MergeRequestDto.getProvider(), oAuth2MergeRequestDto.getProviderUserId());
+//        userRepository.updateOAuth2Info(foundUser.getUserId(), oAuth2MergeRequestDto.getProvider(), oAuth2MergeRequestDto.getProviderUserId());
+        oAuth2UserRepository.insertOAuth2User(oAuth2MergeRequestDto.toOAuth2User(foundUser.getUserId()));
+
         return Map.of(
                 "status", HttpStatus.OK.value(),
                 "code", 2000,
@@ -50,14 +51,14 @@ public class OAuth2AuthService {
     }
 
     public Map<String, Object> signup(OAuth2SignupRequestDto oAuth2SignupRequestDto) {
-        Optional<User> existingUser = userRepository.findByEmail(oAuth2SignupRequestDto.getEmail());
-        if (existingUser != null) {
-            return Map.of(
-                    "status", HttpStatus.CONFLICT.value(),
-                    "code", 4002,
-                    "message", "이미 존재하는 이메일입니다."
-            );
-        }
+//        Optional<User> existingUser = userRepository.findByEmail(oAuth2SignupRequestDto.getEmail());
+//        if (existingUser != null) {
+//            return Map.of(
+//                    "status", HttpStatus.CONFLICT.value(),
+//                    "code", 4002,
+//                    "message", "이미 존재하는 이메일입니다."
+//            );
+//        }
 
         Optional<User> user = userRepository.save(oAuth2SignupRequestDto.toEntity(passwordEncoder));
         Role role = Role.builder()
